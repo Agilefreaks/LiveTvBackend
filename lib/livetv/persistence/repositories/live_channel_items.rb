6b622da1e272ae308b652e52f3d0d330aede1372
Calin <calinoiu.alexandru@agilefreaks.com>
2017-03-08 21:36:42 +0100

    [#20] Add urls to item content

--- lib/livetv/persistence/repositories/live_channel_items.rb
+++ lib/livetv/persistence/repositories/live_channel_items.rb
@@ -9,6 +9,14 @@ module Livetv
   module Persistence
     module Repositories
       class LiveChannelItems
+        def initialize
+          @urls = {}
+          @urls[42] = 'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8'
+          @urls[43] = 'http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8'
+          @urls[44] = 'http://www.streambox.fr/playlists/test_001/stream.m3u8'
+          @urls[45] = 'https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8'
+        end
+
         def current_for(live_channel)
           build_item(0, live_channel)
         end
@@ -19,10 +27,11 @@ module Livetv
 
         def get_content_for(live_channel_item)
           if live_channel_item.id.zero?
-            if live_channel_item.live_channel.id.even?
-              Livetv::Entities::Episode.new(title: 'Some episode', season_number: 1, episode_number: 2, url: 'some url')
+            live_channel = live_channel_item.live_channel
+            if live_channel.id.even?
+              Livetv::Entities::Episode.new(title: 'Some episode', season_number: 1, episode_number: 2, url: @urls[live_channel.id])
             else
-              Livetv::Entities::Movie.new(title: 'Some movie', description: 'The greatest movie of all times', url: 'some url')
+              Livetv::Entities::Movie.new(title: 'Some movie', description: 'The greatest movie of all times', url: @urls[live_channel.id])
             end
           else
             Livetv::Entities::UpcomingItem.new(id: 42)
